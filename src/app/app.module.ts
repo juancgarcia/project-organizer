fbcda9cd70a7100ee5d8f2937fafa176bb8b2440
juancgarcia <juangarcia.info@gmail.com>
2018-07-14 09:33:10 -0400

    Upgrade to Angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +18,7 @@ import { NavComponent } from './components/nav/nav.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     EditorModule,
     AppRoutingModule,
   ],
